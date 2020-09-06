d33bd58a9f9143e51385f4dcae7307ffdbb146a5
cst9221 <chltmdxo3@gmail.com>
2020-09-06 18:58:20 +0900

    add ffmpeg

--- app.js
+++ app.js
@@ -1,23 +1,27 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
+const app = express();
+const port = 5000;
+const server = app.listen(port);
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -39,3 +43,34 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
+new hls(server, {
+  provider: {
+    exists: (req, cd) => {
+      const ext = req.url.split('.').pop();
+
+      // 파일이 존재할 때 null, true 리턴
+      if (ext !== 'm3u8' && ext !== 'ts'){
+        return cd(null, true);
+      }
+
+      fs.access(__dirname + req.url, fs. constants.F_OK, function (err){
+        if (err){
+          console.log('File not exist');
+          return cd(null, false);
+        }
+        cd(null, true);
+      });
+    },
+    // .m3u8파일에 대한 요청일 때 실행
+    getManifestStream: (req, cd) => {
+      const stream = fs.createReadStream(__dirname + req.url);
+      cd(null, stream);
+    },
+    // .ts파일에 대한 요청일 때 실행
+    getaSegmentStream: (req, cd) =>{
+      const stream = fs.createReadStream(__dirname + req.url);
+      cd(null, stream);
+    }
+  }
+})
