cb6103ed9a3e52be196578dbb1012e83f0aba58e
cst9221 <chltmdxo3@gmail.com>
2020-09-15 22:43:21 +0900

    add method setCookie

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,5 +1,15 @@
-function setCookie(username){
-    document.cookie = `Name=${username};  Path='/';`
-    // expires=${date.toUTCString()};
-    alert(username,'asfdsfdfds')
+function setCookie(username) {
+  document.cookie = `username=${username};`;
+  // expires=${date.toUTCString()};
+  let cookies = document.cookie
+    .split(";")
+    .map((cookie) => cookie.split("="))
+    .reduce(
+      (accumulator, [key, value]) => ({
+        ...accumulator,
+        [key.trim()]: decodeURIComponent(value),
+      }),
+      {}
+    );
+console.log(cookies.username)
 }
